795b2a31278d3315aee47f9506009060f24e5784
balogun biola <balogunbiola101@gmail.com>
2019-11-21 19:29:18 +0100

    feature(test): add more reducer test

--- src/__test__/reducers/Todo.test.js
+++ src/__test__/reducers/Todo.test.js
@@ -1,32 +1,69 @@
 import todoReducer from "../../redux/todoReducer";
 
 const newData = {
-    id: 'xs8huqo0fp',
-    dateCompleted: '',
-    isPending: true,
-    isCompleted: false,
-    dueDate: '2019-11-24',
-    title: 'dsddsds'
-  };
+  id: "xs8huqo0fp",
+  dateCompleted: "",
+  isPending: true,
+  isCompleted: false,
+  dueDate: "2019-11-24",
+  title: "dsddsds"
+};
 
 const initialState = {
-    todos: [ ]
-  };
+  todos: []
+};
 
-  describe('todoReducer test', () => {
-    it('should test for the the initial state', () => {
-      expect( todoReducer(undefined, {})).toEqual(initialState);
-    })
+const savedState = {
+  todos: [
+    {
+      id: "xs8huqo0fp",
+      dateCompleted: "",
+      isPending: true,
+      isCompleted: false,
+      dueDate: "2019-11-24",
+      title: "dsddsds"
+    }
+  ]
+};
 
-    it('should handle add todo action', () => {
-        expect(todoReducer(initialState, { type: "ADD_TODOS", payload:  newData  })).toEqual({
-          todos: [newData]
-        });
-      });
+describe("todoReducer test", () => {
+  it("should test for the the initial state", () => {
+    expect(todoReducer(undefined, {})).toEqual(initialState);
+  });
 
-      it('should handle delete todo action', () => {
-        expect(todoReducer(initialState, { type: "DELETE_TODO", payload: newData.id  })).toEqual({
-          todos: [ ] 
-        });
-      });
-})
+  it("should handle add todo action", () => {
+    expect(
+      todoReducer(initialState, { type: "ADD_TODOS", payload: newData })
+    ).toEqual({
+      todos: [newData]
+    });
+  });
+
+  it("should handle delete todo action", () => {
+    expect(
+      todoReducer(initialState, { type: "DELETE_TODO", payload: newData.id })
+    ).toEqual({
+      todos: []
+    });
+  });
+
+  it("should handle update todo action", () => {
+    expect(
+      todoReducer(savedState, {
+        type: "UPDATE_TODOS", 
+        payload: { id: newData.id, title: "new title" , dueDate: "2019-12-24" }
+      })
+    ).toEqual({
+      todos: [
+        {
+          id: "xs8huqo0fp",
+          dateCompleted: "",
+          isPending: true,
+          isCompleted: false,
+          dueDate: "2019-12-24",
+          title: "new title"
+        }
+      ]
+    });
+  });
+});
